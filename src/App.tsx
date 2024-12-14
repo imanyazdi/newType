806a6164c676115a57cf577ec95a8c115cf9b727
Omid <omid.mirzaei2003@gmail.com>
2024-12-14 19:01:38 +0330

    [core] Added Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element="" />
+    </Routes>
+  );
 };
 
 export default App;
